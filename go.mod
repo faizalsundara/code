3abd5aaf83f3f7a6b1ac26531804d4a0d8c343b0
faizal <faizalramadhansundara@gmail.com>
2023-08-17 07:49:30 +0700

    cek init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module leetcode-try
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	test "leetcode-try/easy"
+)
+
+func main() {
+	//0
+	fmt.Println(test.Palindrome(`kupu-kupu`))
+	fmt.Println(test.Palindrome(`malam`))
+	fmt.Println(test.Palindrome(`civic`))
+	// //1
+	fmt.Println(test.TwoSum1([]int{2, 7, 8, 5}, 9))
+	// fmt.Println(twoSum1([]int{2, 7, 5, 6, 5}, 10))
+	// //2
+	// fmt.Println(isPalindrome(10))
+	// //3
+	// fmt.Println(romanNumber("MCMXCIV"))
+	// //7
+	// fmt.Println(reverseInteger(1534236469))
+	// //20
+	// fmt.Println(reverseInteger(1534236469))
+}
